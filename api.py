b46eaad3318d6e4bce139398360276bfbc9ea737
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2023-12-20 16:38:18 -0500

    test

--- api.py
+++ api.py
@@ -168,4 +168,4 @@ async def publish_draft(story_request: StoryRequest, current_user: UserInDB = De
     return {"story_id":story_id}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile="/etc/letsencrypt/live/banglallm.rashik.sh/fullchain.pem", ssl_certfile="/etc/letsencrypt/live/banglallm.rashik.sh/privkey.pem")
