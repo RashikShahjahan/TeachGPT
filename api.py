510aa0d342e53b679c40e8215d314eaaef137443
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2023-12-20 20:57:41 -0500

    test

--- api.py
+++ api.py
@@ -18,6 +18,9 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:3000",
+    "https://localhost",
+    "https://localhost:8000",
+    "https://localhost:3000",
 ]
 
 app.add_middleware(
