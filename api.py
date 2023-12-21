c284ace589294d7122b4bcc25c167ef8bf7611a0
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2023-12-21 01:16:15 -0500

    test

--- api.py
+++ api.py
@@ -102,7 +102,7 @@ async def generate_invite_code_endpoint():
     invite_codes_collection.insert_one({"code": invite_code, "used": False})
     return {"invite_code": invite_code}
 
-@app.post("token")
+@app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
@@ -132,7 +132,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise HTTPException(status_code=401, detail="Invalid token")
     return user
 
-@app.post("register")
+@app.post("/register")
 async def register(user_create: UserCreate):
     # Check if the invite code is valid and not used
     invite_code_record = invite_codes_collection.find_one({"code": user_create.inviteCode, "used": False})
@@ -151,7 +151,7 @@ async def register(user_create: UserCreate):
     return {"username": user_create.username}
 
 
-@app.get("draft")
+@app.get("/draft")
 async def get_draft():
     draft = get_single_story()
     if draft:
@@ -160,7 +160,7 @@ async def get_draft():
         return {"draft":"No drafts to edit!"}
 
 
-@app.post("draft")
+@app.post("/draft")
 async def publish_draft(story_request: StoryRequest, current_user: UserInDB = Depends(get_current_user)):
     story_id = str(write_story_to_mongodb(story_request.story))
     return {"story_id":story_id}
